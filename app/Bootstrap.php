4053951148747a0125ad082189fda7a07c6d7903
Adam Carruthers <adam@bitjutsu.ca>
2014-10-30 09:08:07 -0400

    Update database initialization

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -2,6 +2,18 @@
 
 # Set up constants:
 
+# Create the database:
+$db = new SQLite3(dirname(__FILE__).'/../data/sqlite.prod');
+$db->exec(
+    "CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY,
+        title STRING NOT NULL,
+        body STRING NOT NULL,
+        date_created INTEGER NOT NULL,
+        date_modified INTEGER NOT NULL
+    )"
+);
+$db->close();
 
 # Include the Layout - will be the entry point to the app:
 include 'Layout.php';
